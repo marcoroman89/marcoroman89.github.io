97884057d6ca195167989ebc3f679de60dd4383f
Marco Roman <marco.roman89@gmail.com>
2014-07-17 04:42:09 -0500

    updated scrolling for mobile devices.

--- js/smooth-scrolling.js
+++ js/smooth-scrolling.js
@@ -57,3 +57,23 @@ $("a.btn[href^='#']").on('click', function(e) {
      });
 
 });
+
+$("span.scroll p a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top -80
+     }, 900, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
